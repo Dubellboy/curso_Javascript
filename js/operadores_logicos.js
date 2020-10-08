69824d3fe9ffb2970336f83e40951eb306d5f345
Eduardo Chagas <eduardo_cns@hotmail.com>
2020-10-08 18:48:02 -0300

    Inicio operadores lógicos

---
+++ js/operadores_logicos.js
@@ -0,0 +1,5 @@
+//Operadores lógicos 
+// && => Significa  e as duas expressões devem ser true, 
+// || => Significa ou e uma das expressões deve ser true,
+//  ! => Significa negação de comparação
+
