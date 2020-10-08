eae98539906c71e714d1f2c4ddaeed046197d60f
Eduardo Chagas <eduardo_cns@hotmail.com>
2020-10-08 18:46:39 -0300

    Inicio operadores lógicos

--- js/if_else.js
+++ js/if_else.js
@@ -52,3 +52,44 @@ if(8 < 14){
 }else{
     document.write(false);
 }
+
+document.write('<hr />');
+
+//var nota = prompt('Digite sua nota');
+// var media = 7;
+
+// if (nota >= media) {//No caso a nota ter sido inputada pelo prompt o javascript converte em número devido ao operador lógico 
+//     document.write('Aprovado')
+// }else {
+//     document.write('Reprovado')
+// }
+
+document.write('<hr />');
+
+
+
+// var variavel1 = prompt('Digite algum numero:');
+// var variavel2 = prompt('Digite otro número:');
+
+// //Nesse caso ira concatenar os números não somalos, 
+// //pois estão como strings
+// //document.write(variavel1 + variavel2);
+
+document.write('<hr />');
+
+//Casting é transformar um tipo de dado em outro tipo de dado
+//Ao colocar parseInt ou parseFloar ele converte o dado.
+// var variavel3 = parseInt(prompt('Digite algum numero:'));
+// var variavel4 = parseInt(prompt('Digite otro número:'));
+
+//document.write(variavel3 + variavel4);
+
+document.write('<hr />');
+
+//Converter números em strings
+
+var n1 = 10;
+var n2 = 20;
+
+//colocar toString()
+document.write(n1.toString() + n2.toString());
