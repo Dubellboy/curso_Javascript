05e702773a751bee42aced20e496d32af72ee98e
Eduardo Chagas <eduardo_cns@hotmail.com>
2020-10-08 13:20:36 -0300

    fim aula de variáveis

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*--------Uso de variáveis--------*/
+
+//String entre "" ou ''
+var texto = 'Curso de Javascript';
+ 
+
+//Number podendo ser inteiro ou fracionado
+var numeroInteiro = -7;
+var numeroFracionado = 11210.75;
+
+
+//Booleans - true ou false
+var teste =true
+   
+
+//Abrir com uma caixa de diálogo
+alert(texto);
+
+//Aparecer no browser
+document.write(texto);
+
+//Aparecer no console do browser
+console.log(teste);
